f65170319e238c6ee4db992239b8128f48c1d8d6
levant47 <levant.ba@gmail.com>
2023-06-02 10:47:45 +0300

    M13

--- CatalogService/Tests/TestsBase.cs
+++ CatalogService/Tests/TestsBase.cs
@@ -3,5 +3,5 @@
     protected MySqlConnection _connection;
 
     [SetUp]
-    public void SetUp() => _connection = new("server=127.0.0.1;uid=root;pwd=root;database=test");
+    public void SetUp() => _connection = new("server=127.0.0.1;uid=root;database=test");
 }
